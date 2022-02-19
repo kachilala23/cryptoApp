f302e6a27782591579b06075522757e42221b064
kachilala <kachilala23@gmail.com>
2022-02-19 09:41:33 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CryptoDetail, Transaction } from "./screens";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Tabs from "./navigation/tabs";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'Home'}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Tabs}
+        />
+        <Stack.Screen
+          name="CryptoDetail"
+          component={CryptoDetail}
+        />
+        <Stack.Screen
+          name="Transaction"
+          component={Transaction}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
